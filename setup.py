a19f674eb2a6cfdfce7ff2345c03ddec60c2f475
Helmut Konrad Schewe <helmutus@outlook.com>
2022-12-20 23:42:06 +0000

    chore(setup): use quick installer

--- setup.py
+++ setup.py
@@ -8,48 +8,18 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
-import os
-import re
+import utila
 
-import setuptools
-
-ROOT = os.path.abspath(os.path.dirname(__file__))
-
-with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
-    README = fp.read()
-
-with open(os.path.join(ROOT, 'footnote/__init__.py'), encoding='utf8') as fp:
-    VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
-
-with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
-    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
+PACKAGES = [
+    'footnote',
+    'footnote.feature',
+    'footnote.parser',
+    'footnote.strategy',
+    'footnote.strategy.moving',
+]
+ENTRY_POINTS = dict(console_scripts=[
+    'footnote = footnote.cli:main',
+])
 
 if __name__ == "__main__":
-    # allow setup.py to run from another directory
-    os.chdir(ROOT)
-    setuptools.setup(
-        author='Helmut Konrad Fahrendholz',
-        author_email='info@checkitweg.de',
-        description='just the best',
-        install_requires=REQUIRES,
-        long_description=README,
-        name='footnote',
-        platforms='any',
-        url='https://dev.package.checkitweg.de/footnote',
-        version=VERSION,
-        zip_safe=False,  # create 'zip'-file if True. Don't do it!
-        classifiers=[
-            'Programming Language :: Python :: 3.8',
-            'Programming Language :: Python :: 3.9',
-        ],
-        packages=[
-            'footnote',
-            'footnote.feature',
-            'footnote.parser',
-            'footnote.strategy',
-            'footnote.strategy.moving',
-        ],
-        entry_points={
-            'console_scripts': ['footnote = footnote.cli:main',],
-        },
-    )
+    utila.install(__file__)
